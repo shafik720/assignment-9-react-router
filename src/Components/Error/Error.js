85b34b171af502fbba7cb46aa5537a06de8301ef
shafik720 <shafikrasel5@gmail.com>
2022-11-02 22:42:48 +0600

    dfgdsg

---
+++ src/Components/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Error.css'
+
+const Error = () => {
+    return (
+        <div className="error-div">
+            <h2>Error 404 Not Found</h2>
+        </div>
+    );
+};
+
+export default Error;
