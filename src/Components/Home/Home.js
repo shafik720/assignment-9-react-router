8c8b8f03ddb9a511850e7705437c230d5e493ef2
shafik720 <shafikrasel5@gmail.com>
2022-11-03 00:17:40 +0600

    icon added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Home.css';
-import img1 from '../../utilities/img/dragon (1).png'
+import img1 from '../../utilities/img/dragon.png'
 
 const Home = () => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row home-div">
                 <div className="col-lg-6">
-                    <div className="">
-                        <h2>Welcome To Dragon's Club</h2>
+                    <div className="home-left h-100">
+                        <h1>Welcome To <span className="red-text"> Dragon</span>'s Club</h1>
+                        <h2>Buy a Dragon</h2>
+                        <h2>Ride a Dragon</h2>
+                        <h2>And Be a Dragon</h2>
+                        <button>Show Me, I am Excited</button>
                     </div>
                 </div>
                 <div className="col-lg-6">
