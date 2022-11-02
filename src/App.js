05e577c5b89fdfc1001b7414bb1df5ce5cefec43
shafik720 <shafikrasel5@gmail.com>
2022-11-02 22:41:04 +0600

    dfgdsg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path="/Home"></Route>
+      </Routes>
     </div>
   );
 }
