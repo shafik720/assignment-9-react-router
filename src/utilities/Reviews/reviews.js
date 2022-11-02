62abe676520ad323486f58b8f60d61dcf70777c9
shafik720 <shafikrasel5@gmail.com>
2022-11-03 01:31:40 +0600

    error page done

---
+++ src/utilities/Reviews/reviews.js
@@ -0,0 +1,12 @@
+
+
+let userReviews = [
+    {
+        id: 1, 
+        name : 'Andrew Garfield',
+        profession : 'Actor',
+        location : 'California',
+        img : '',
+        comments : 'This Dragon is Amazing. I bought her 2 Months ago. I never had to fly plane after i owned her. She is smart and sweet. She never grew tired while flying. She always listen to me. I am so glad that i have her. I love her so much. Thanks to the seller for showing her to me.'
+    }
+]
