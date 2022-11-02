a65e6fa77fc96daa3c4300d82035596fb1faa395
shafik720 <shafikrasel5@gmail.com>
2022-11-02 22:47:46 +0600

    route on app.js setup done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import { Route, Routes } from 'react-router';
+import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
+import Error from './Components/Error/Error';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path="/Home"></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<Error></Error>}></Route>
       </Routes>
     </div>
   );
