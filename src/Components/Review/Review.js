05ca736359b1c1e450a0b3fb7b7ea5436e0f07ac
shafik720 <shafikrasel5@gmail.com>
2022-11-03 08:32:17 +0600

    dfgdsg

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Review.css';
 
-const Review = () => {
+const Review = (props) => {
+    const{ img, userName, location, profession, comments} = props.index;
+    console.log(props.index);
     return (
-        <div>
-            <h2>User Review</h2>
+        <div className=" col-lg-4">
+            <div className="user-card">
+                <div className="userCard-top">
+                    <div className="userImg">
+                        <img src={'/img/user.jpg'} alt=""/>
+                    </div>              
+                    <div className="user-details">
+                        <p>Name : {userName} </p>
+                        <p>{location}</p>
+                    </div>
+                </div>
+                <div className="user-comments">
+                    <p>{comments}</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from '../Review/Review';
 import './Reviews.css'
 
 const Reviews = () => {
+    let [users, setUsers] = useState([]);
+    useEffect(()=>{
+        fetch('userDb.json')
+        .then(res=>res.json())
+        .then(data=>setUsers(data))
+    },[])
     return (
-        <div>
-            <h2>Review Here</h2>
+        <div className="container review-div">
+            <div className="row">
+            {
+                users.map(index=><Review
+                    index={index}
+                    key={index.id}
+                ></Review>)
+                }
+            </div>
+            
         </div>
     );
 };
