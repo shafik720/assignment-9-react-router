4875e5e17a4e022a79fa513a34a239c15bba864a
shafik720 <shafikrasel5@gmail.com>
2022-11-04 23:26:28 +0600

    working on chart

--- src/Components/SecondChart/SecondChart.js
+++ src/Components/SecondChart/SecondChart.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Bar, BarChart, ResponsiveContainer } from 'recharts';
 
 const SecondChart = (props) => {
     let data = props.chart;
+    console.log(data);
     return (
         <div>
             <h4 className="text-center mt-5">Second Chart</h4>
             <hr width="50%" className="mx-auto mb-4" />
+            <div className="container">
+                <div className="row">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart width={150} height={40} data={data}>
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
+                </div>
+            </div>
         </div>
     );
 };
