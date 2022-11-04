49c6250599087abaa8f1280d6e3029705a8c1554
shafik720 <shafikrasel5@gmail.com>
2022-11-04 22:37:03 +0600

    rechart work is going

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FirstChart from '../FirstChart/FirstChart';
 import './Dashboard.css'
 
 const Dashboard = () => {
+    const[chart, setChart] = useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setChart(data))
+    },[])
     return (
         <div className="dashBoard-div">
-            <h2>Dash Board Section</h2>
+            
         </div>
     );
 };
---
+++ src/Components/FirstChart/FirstChart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FirstChart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FirstChart;
