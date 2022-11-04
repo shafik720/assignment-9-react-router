d78ee5ea2bd840a90ca8a6cd841b65aeac6fc9e1
shafik720 <shafikrasel5@gmail.com>
2022-11-04 22:40:23 +0600

    rechart work is going

--- src/Components/FirstChart/FirstChart.js
+++ src/Components/FirstChart/FirstChart.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const FirstChart = (props) => {
     console.log(props.chart);
+    let data = props.chart;
     return (
         <div>
             <h2>Hello From Chart</h2>
