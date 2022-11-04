953e3fe896d50afcd3efb2ef78375716731c3449
shafik720 <shafikrasel5@gmail.com>
2022-11-04 23:28:31 +0600

    working on chart

--- src/Components/SecondChart/SecondChart.js
+++ src/Components/SecondChart/SecondChart.js
@@ -9,12 +9,10 @@ const SecondChart = (props) => {
             <h4 className="text-center mt-5">Second Chart</h4>
             <hr width="50%" className="mx-auto mb-4" />
             <div className="container">
-                <div className="row">
-                <ResponsiveContainer width="100%" height="100%">
-                    <BarChart width={150} height={40} data={data}>
-                    <Bar dataKey="investment" fill="#8884d8" />
+                <div className="row mb-5">
+                    <BarChart className="mx-auto" width={800} height={400} data={data}>
+                        <Bar dataKey="investment" fill="#8884d8" />
                     </BarChart>
-                </ResponsiveContainer>
                 </div>
             </div>
         </div>
