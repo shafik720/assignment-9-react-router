fa1272635926a4a5d0bf0fbd65e6990fc8223697
shafik720 <shafikrasel5@gmail.com>
2022-11-04 12:50:58 +0600

    product on review page adding

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './Review.css';
 
 const Review = (props) => {
-    const{ img, userName, location, profession, comments} = props.index;
+    const{ img, userName, location, profession, comments, dragonName, imgSrc, price, ratings} = props.index;
+    console.log(props.index);
     return (
         <div className=" col-lg-4 review-divs">
             <div className="user-card">
+                <div className="productCard">
+                    <div className="productCard-top">
+                        <img src={imgSrc} alt=""/>
+                    </div>
+                    <div className="productCard-details">
+                        <div className="details-left">
+                            <p>{dragonName}</p>
+                        </div>
+                        <div className="details-right">
+                            <p>Price : {price} BTC </p>
+                            <p>Ratings : {ratings} </p>
+                        </div>
+                    </div>
+                </div>
                 <div className="userCard-top">
                     <div className="userImg">
                         <img src={img} alt=""/>
