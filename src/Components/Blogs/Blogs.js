65546f5a51ba643464e7e53cbf3307d71aef7d1a
shafik720 <shafikrasel5@gmail.com>
2022-11-05 10:39:39 +0600

    working on blog sections

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,12 +4,16 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div className="blogs-div">
-            <div className="container">
-                <div className="row">
+            <div className="container my-5">
+                <div className="row mt-5">
                     <div className="col-lg-1"></div>
                     <div className="col-lg-5">
                         <div className="">
-                            <h2>Context Api</h2>
+                            <h4 className="fw-bold fw-bolder">Context Api কি ?</h4>
+                            <hr />
+                            <p>Prop Drilling এড়াতে Context api ব্যবহার করা হয়। 
+                                সাধারনত রিয়েক্ট এপে একটা কম্পোনেন্টের কোন ভ্যালুকে অন্য কম্পোনেন্টে ইউজ করতে হলে prop ইউজ করতে হয়। দুরবর্তী কোন child কম্পোনেন্টে এই ভ্যালু ইউজ  করা লাগলে মধ্যবর্তী সকল চাইল্ড কম্পোনেন্টে এই prop বারবার ইউজ করা লাগে, এটাকে prop drilling বলে এবং এটা সময় সাপেক্ষ ও বিরক্তিকর। এই প্রপ ড্রিলিং ঝামেলা এড়াতে, একটা ভ্যালুকে prop ছাড়াই গ্লোবালি ইউজ করতে context api ব্যবহার করা হয়। 
+                            </p>
                         </div>
                     </div>
                 </div>
